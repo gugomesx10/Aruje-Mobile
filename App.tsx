4115335866a3559303e92d420cc48df45093b792
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-06-22 14:42:42 -0300

    feat(auth-login): mantém usuário logado no app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+
 import { LoginScreen } from "./src/screens/LoginScreen";
 import { MainTabs } from "./src/navigation/MainTabs";
+import { getToken } from "./src/storage/authStorage";
+import { colors } from "./src/theme/colors";
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loadingAuth, setLoadingAuth] = useState(true);
+
+  useEffect(() => {
+    async function loadStoredToken() {
+      try {
+        const token = await getToken();
+
+        if (token) {
+          setIsAuthenticated(true);
+        }
+      } finally {
+        setLoadingAuth(false);
+      }
+    }
+
+    loadStoredToken();
+  }, []);
+
+  if (loadingAuth) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: colors.background,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicator size="large" color={colors.primary} />
+        <StatusBar style="dark" />
+      </View>
+    );
+  }
 
   return (
     <>
